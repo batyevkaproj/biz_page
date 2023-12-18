d4e486e5505eec9341f84258e3674a96cd62195d
Валера <i.radetskyi@batyevka.net>
2023-12-18 15:33:02 +0200

    o br e v u pt bk na 4 sto
    viva la front endio

---
+++ landing_pace/landing-app/src/views/Master.vue
@@ -0,0 +1,141 @@
+<template>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+</head>
+<body>
+  <header>
+    <div class="mobile">
+        <p class="p-mobile-top">Звони нам! У нас<br>быстрый дозвон,<br>живые операторы</p>
+        <a class="tel tel-mobile" href=""><img class="small-icon chat-icon" src="img/msg_small.svg" width="16px" height="16px">0 800 30 32 30</a>
+    </div>
+    <div class="mid-mobile">
+        <a class="logo-mobile" href="/"><img src="img/logo_mobile.svg" alt=""></a>
+        <a class="burger" data-micromodal-trigger="modal-1">
+            <span class="firstOverlayLine spann-mobile"></span>
+            <span class="secondOverlayLine spann-mobile"></span>
+            <span class="thirdOverlayLine spann-mobile"></span>    
+        </a>
+    </div>
+    <div class="bottom-mobile">
+        <h5 class="RandiesChristmassSpecialOffer"><span class="fat">Спецпредложение!</span><br>Бандитский интернет Yahoo4it<br>не по-детски!</h5>
+    </div>
+    <div class="second-bottom-mobile">
+        <button class="connect-mobile">Заявка на підключення</button>
+    </div>
+    <button data-custom-close="modal-1">close</button>
+
+    <a href="#" data-micromodal-trigger="modal-1"></a>
+    <div id="modal-1" aria-hidden="true">
+
+        <div class="mid-right-options-modal">
+            <a href="" class="grn"><img class="med-icon-modal" src="img/wallet.svg" width="32px" height="32px">Оплата</a>
+            <a href="" class="blue"><img class="med-icon-modal" src="img/247.svg" width="32px" height="32px">Підтримка</a>
+            <a href="" class="gray"><img class="med-icon-modal" src="img/gear.svg" width="32px" height="32px">Опції</a>
+        </div>
+
+        <div class="boyan boyan-1"><img class="boyan-image" src="img/high-buildings.svg" alt=""><a href="/">Для Багатоповерхівок</a></div>
+
+        <div class="forBoyanMenu">
+
+        </div>
+
+        <div class="boyan boyan-2"><img class="boyan-image boyan-image-private" src="img/private_sector.svg" alt=""><a href="/">Приватним Будинкам</a></div>
+
+        <div class="forBoyanMenu">
+
+        </div>
+
+        <div class="boyan boyan-3"><img class="boyan-image" src="img/buiss.svg" alt=""><a href="/">Для Бізнеса</a></div>
+
+        <div class="forBoyanMenu">
+
+        </div>
+
+    </div>
+
+
+    <div class="top">
+        <nav class="left-top">
+            <a href="">Акції</a>
+            <a href="">Карта покриття</a>
+        </nav>
+        <nav class="mid-top">
+            <a class="a mobile-880" href=""><label class="mobile-1024">Звони нам! </label>У нас быстрый дозвон<label class="mobile-1024">, живые операторы</label></a>
+            <a class="tel" href=""><img class="small-icon chat-icon" src="img/msg_small.svg" width="16px" height="16px">0 800 30 32 30</a>
+            <a class="tel-arrow" href="/"><img class="small-icon small-icon-arrow" src="img/arrow_down.svg"></a>
+        </nav>
+        <nav class="right-top">
+            <a href=""><img class="small-icon" src="img/ket_small.svg" width="16px" height="16px">Війти</a>
+            <a href=""><img class="small-icon" src="img/globe_small.svg" width="16px" height="16px">Укр</a>
+        </nav>
+    </div>
+    <div class="mid">
+        <nav class="mid-left">
+            <a class="logo" href="/"><img src="img/logo.svg" alt=""></a>
+            <a class="banner" href="/"><img src="img/bandit-ban.svg" alt=""></a>
+        </nav>
+        <a href="" class="grn mid-right-options-mobile mobile-800"><img class="" src="img/wallet.svg" width="32px" height="32px"></a>
+        <a href="" class="blue mid-right-options-mobile mobile-800"><img class="" src="img/247.svg" width="32px" height="32px"></a>
+        <a href="" class="gray mid-right-options-mobile mobile-800"><img class="" src="img/gear.svg" width="32px" height="32px"></a>
+        <nav class="mid-right">
+            <div class="mid-right-options hide-1280">
+                <a href="" class="grn"><img class="med-icon" src="img/wallet.svg" width="32px" height="32px">Оплата</a>
+                <a href="" class="blue"><img class="med-icon" src="img/247.svg" width="32px" height="32px">Підтримка</a>
+                <a href="" class="gray"><img class="med-icon" src="img/gear.svg" width="32px" height="32px">Опції</a>
+            </div>
+                <button class="connect">Заявка на підключення</button>
+                <a class="burger" href="/" data-micromodal-trigger="modal-1">
+                    <span class="firstOverlayLine spann"></span>
+                    <span class="secondOverlayLine spann"></span>
+                    <span class="thirdOverlayLine spann"></span>
+                </a>
+        </nav>
+    </div>
+    <div class="bottom">
+        <a class="mobile-1420" href="">Для многоэтажек</a>
+        <a class="mobile-1420" href="">Частным домам</a>
+        <a class="mobile-1420-margin-left" href="">Для бизнеса</a>
+        <img src="img/rectangle.svg" alt="">
+        <a href="">Доступ  в <br> Интернет</a>
+        <a class="mobile-800" href="">Трансляция ТВ и футбола <br> для зведений</a>
+        <a class="mobile-1024" href="">Строительство локальных <br> сетей для офисов</a>
+        <a class="mobile-1420" href="">Видео наблюдение <br> для бизнеса</a>
+        <a href="">Все услуги</a>
+    </div>
+  </header>
+
+
+
+  <script src="https://unpkg.com/micromodal@0.4.10/dist/micromodal.min.js"></script>
+  <script>
+    MicroModal.init({
+        onShow: modal => console.info(`${modal.id} is shown`), // [1]
+        onClose: modal => console.info(`${modal.id} is hidden`), // [2]
+        openTrigger: 'data-custom-open', // [3]
+        closeTrigger: 'data-custom-close', // [4]
+        openClass: 'is-open', // [5]
+        disableScroll: true, // [6]
+        disableFocus: false, // [7]
+        awaitOpenAnimation: false, // [8]
+        awaitCloseAnimation: false, // [9]
+        debugMode: true // [10]
+    });
+  </script>
+</body>
+</html>
+
+</template>
+
+<script>
+export default {
+    name:'Master',
+};
+</script>
+
+<style>
+</style>
